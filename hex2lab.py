0e9595d44cda39016462fa5c76c39f77fed8dc53
yasu4155 <yasu4155@gmail.com>
2025-03-03 12:28:23 +0900

    Update hex2lab.py

--- hex2lab.py
+++ hex2lab.py
@@ -16,9 +16,11 @@ def main():
     # === HEXカラーリスト（指定された色） ===
     # hex_colors = init_colors()
     hex_color = '#808080'
-    hex_color, flag = pick_rgb()
-    hex_color, flag = input_rgb()
-
+    if st.sidebar.checkbox('Input RGB')
+    	hex_color, flag = input_rgb()
+    if st.sidebar.checkbox('Pick color')
+    	hex_color, flag = pick_rgb()
+    
     button = st.sidebar.button('Clear')
     if button:	  
         st.session_state.hex_colors = []
