01757a97567d619bc3def7926f3d597b0ae178e9
yasu4155 <yasu4155@gmail.com>
2025-03-03 15:25:29 +0900

    Update hex2lab.py

--- hex2lab.py
+++ hex2lab.py
@@ -15,8 +15,8 @@ def main():
 
     if st.sidebar.button('Clear'):
         st.session_state.hex_colors = []
-    if st.session_state.hex_colors:
-        if st.sidebar.button('Undo'):
+    if st.sidebar.button('Undo'):
+        if st.session_state.hex_colors:
             st.session_state.hex_colors.pop()
     button = st.sidebar.button('Plot')
 
