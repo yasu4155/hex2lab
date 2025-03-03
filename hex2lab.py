f1cb9ceaf5b786a6fd73cb0756a831b1d67c2b7f
yasu4155 <yasu4155@gmail.com>
2025-03-03 14:17:45 +0900

    Update hex2lab.py

--- hex2lab.py
+++ hex2lab.py
@@ -18,8 +18,9 @@ def main():
         st.session_state.hex_colors = []
     button = st.sidebar.button('Undo')
     if button:
+        st.write(st.session_state.hex_colors)
         hex_colors = st.session_state.hex_colors.pop(0)
-        st.session_state.hex_colors = hex_colors   
+        st.write(hex_colors)
     button = st.sidebar.button('Plot')
 
     # === HEXカラーリスト設定 ===
