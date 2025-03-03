32be1d8150f8a8954f38328d61dd87dfdcdb4f7d
yasu4155 <yasu4155@gmail.com>
2025-03-03 14:38:59 +0900

    Update hex2lab.py

--- hex2lab.py
+++ hex2lab.py
@@ -50,7 +50,7 @@ def main():
 # === RGBカラーを入力する関数 ===
 def input_rgb():
     hex_color = '#808080'
-    sel = st.sidebar.radio(,('RGB input', 'Color picker'))
+    sel = st.sidebar.radio('', ('RGB input', 'Color picker'))
     if sel == 'RGB input':
        r = st.sidebar.slider('Red',   0, 255, 128,)
        g = st.sidebar.slider('Green', 0, 255, 128,)
